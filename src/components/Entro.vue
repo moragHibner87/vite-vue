f16b2ef5f8eac226ebe64313a1e48849e045e668
morag <morage.d@gmail.com>
2023-04-21 01:45:55 +0300

    for loop

--- src/components/Entro.vue
+++ src/components/Entro.vue
@@ -1,12 +1,15 @@
 <template>
    <section class="entro sec--pad">
         <div class="inner_width text-center">
-        <p ref="fadeEl" class="enter-up">
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy
-        </p>
-        <div class="card" ref="card">
-            <h2>Glow me on</h2>
-        </div>
+            <p ref="fadeEl" class="enter-up">
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy
+            </p>
+            <div class="card" ref="card">
+                <h2>Glow me on</h2>
+            </div>
+            <div class="snake">
+                <span></span><span></span><span></span><span></span><span></span><span></span>
+            </div>
         </div>
     </section>
 </template>
